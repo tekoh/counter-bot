825eae03e2cbf482d9b9a7f1b02290fdd7f89f2b
tekoh <max.0x71@gmail.com>
2021-12-08 22:28:47 +0000

    guildcreate event

---
+++ events/guildCreate.js
@@ -0,0 +1,11 @@
+const { Guild, Client } = require("discord.js")
+const { info, types } = require("../utils/logger")
+
+/**
+ * @param {Client} client
+ * @param {Guild} guild
+ */
+module.exports = async (client, guild) => {
+    info(`added to ${guild.name} (${guild.id}) new count: ${client.guilds.cache.size}`, types.GUILD)
+    runCheck(guild)
+}
