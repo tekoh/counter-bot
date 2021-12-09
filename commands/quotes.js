b082017eebd0c19c3766d314223801d652a1ab88
max <max.0x71@gmail.com>
2021-12-09 13:13:14 +0000

    quotes command

---
+++ commands/quotes.js
@@ -0,0 +1,31 @@
+const { Command, categories } = require("../utils/classes/Command")
+const { Message } = require("discord.js")
+const { ErrorEmbed, CustomEmbed } = require("../utils/classes/EmbedBuilders")
+const { getQuotes } = require("../utils/utils")
+
+const cmd = new Command("quotes", "list all quotes and their uses", categories.FUN)
+
+/**
+ *
+ * @param {Message} message
+ * @param {Array<String>} args
+ */
+async function run(message, args) {
+    const quotes = getQuotes()
+
+    if (quotes.length == 0) {
+        return message.channel.send({embeds: [new ErrorEmbed("no quotes available")]})
+    }
+
+    let msg = ""
+
+    for (const quote of quotes) {
+        msg += `**${quote.quote}** \`${quote.mentions.toLocaleString()} uses\`\n`
+    }
+
+    return message.channel.send({embeds: [new CustomEmbed(msg)]})
+}
+
+cmd.setRun(run)
+
+exports = cmd
