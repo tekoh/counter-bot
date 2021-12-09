cd400681360b224424ccfe2bd0b5eb926b399f70
max <max.0x71@gmail.com>
2021-12-09 13:10:37 +0000

    deletequote command

---
+++ commands/deletequote.js
@@ -0,0 +1,29 @@
+const { Command, categories } = require("../utils/classes/Command")
+const { Message } = require("discord.js")
+const { ErrorEmbed, CustomEmbed } = require("../utils/classes/EmbedBuilders")
+const { quoteExists, deleteQuote } = require("../utils/utils")
+
+const cmd = new Command("deletequote", "delete a quote", categories.FUN).setAliases(["delquote"])
+
+/**
+ *
+ * @param {Message} message
+ * @param {Array<String>} args
+ */
+async function run(message, args) {
+    if (args.length == 0) {
+        return message.channel.send({ embeds: [new ErrorEmbed("whats the quote dumbass")] })
+    }
+
+    const quote = args.join(" ")
+
+    if (!quoteExists(quote)) return message.channel.send({ embeds: [new ErrorEmbed("that quote doesnt exist")] })
+
+    deleteQuote(quote)
+
+    return message.channel.send({ embeds: [new CustomEmbed("quote deleted xoxo")] })
+}
+
+cmd.setRun(run)
+
+exports = cmd
