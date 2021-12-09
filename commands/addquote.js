0648fb4366206a9db107f6d2d83d4a557574f8ed
max <max.0x71@gmail.com>
2021-12-09 13:09:40 +0000

    addquote command

---
+++ commands/addquote.js
@@ -0,0 +1,29 @@
+const { Command, categories } = require("../utils/classes/Command")
+const { Message } = require("discord.js")
+const { ErrorEmbed, CustomEmbed } = require("../utils/classes/EmbedBuilders")
+const { quoteExists, addQuote } = require("../utils/utils")
+
+const cmd = new Command("addquote", "add a quote to be counted", categories.FUN)
+
+/**
+ *
+ * @param {Message} message
+ * @param {Array<String>} args
+ */
+async function run(message, args) {
+    if (args.length == 0) {
+        return message.channel.send({embeds: [new ErrorEmbed("whats the quote dumbass")]})
+    }
+
+    const quote = args.join(" ")
+
+    if (quoteExists(quote)) return message.channel.send({ embeds: [new ErrorEmbed("that quote already exists")] })
+    
+    addQuote(quote)
+
+    return message.channel.send({embeds: [new CustomEmbed("quote added xoxo")]})
+}
+
+cmd.setRun(run)
+
+exports = cmd
